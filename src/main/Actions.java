bb930e6c35a7a2ec11a3f42e957188270a1d1bb0
Em-kale <e.luhning@vitalpoint.ai>
2020-11-07 12:51:39 -0500

    reconfigured Attack class to remove console related actions, and communicate with Event

--- src/main/Actions.java
+++ src/main/Actions.java
@@ -9,19 +9,17 @@ import java.util.*;
 public class Actions {
     private final GameBoard gameBoard;
     private final ArrayList<Player> players;
-    private final Parser parser;
     private Player activePlayer;
     private int activePlayerIndex;
 
     /**
      * Constructor for Actions objects
      *
-     * @param players A list of players who will be playing the game
+     * @param players   A list of players who will be playing the game
      * @param gameBoard The digital game board that will be used to play the game
      */
     public Actions(ArrayList<Player> players, GameBoard gameBoard) {
         this.players = players;
-        this.parser = new Parser();
         this.gameBoard = gameBoard;
         this.activePlayerIndex = 0;
         this.activePlayer = players.get(activePlayerIndex);
@@ -37,7 +35,7 @@ public class Actions {
     private void initialArmyAllocation() {
         int armies = 0;
 
-        switch (players.size()){
+        switch (players.size()) {
             case 2:
                 armies = 5;
                 break;
@@ -57,7 +55,7 @@ public class Actions {
                 break;
         }
 
-        for(Player p : players) {
+        for (Player p : players) {
             p.setArmies(armies);
         }
     }
@@ -76,7 +74,7 @@ public class Actions {
         }
 
         // Randomly give players territories and allot initial armies.
-        for (int i = 0; i < numPlayers; i++){
+        for (int i = 0; i < numPlayers; i++) {
             territory = gameBoard.getUnallocatedTerritory();
 
             // If there are no territories left to assign quit
@@ -114,7 +112,7 @@ public class Actions {
             // If it is the last player reset it back to the start
             if (i == numPlayers - 1) {
                 // If the last player has no armies we are done
-                if (player.getArmies() == 0){
+                if (player.getArmies() == 0) {
                     break;
                 }
                 i = -1; // Must be -1 because the for loop will increase it by one
@@ -124,103 +122,14 @@ public class Actions {
 
     /**
      * Attacks an enemy territory from an adjacent owned territory, and resolves the outcome of the attack.
+     *
      * @param defenderTerritory The territory being attacked
      */
-    private void attack(String defenderTerritory)
-    {
-        Scanner input = new Scanner(System.in);
+    public void attack(String attackerTerritory, String defenderTerritory, int attackersDice, int defendersDice) {
         String currPlayer = activePlayer.getName();
-        String attackerTerritory;
 
-        // Checks if you own the territory if you do return.
-        if (gameBoard.getTerritoryRuler(defenderTerritory).equals(currPlayer)) {
-            System.out.println("You cannot attack your own territory.");
-            return;
-        }
 
-        System.out.println("Which territory would you like to attack from?");
-
-        // If you have no adjacent territories return.
-        Set<String> adjacentTerritories = gameBoard.getAdjacentTerritories(defenderTerritory);
-        boolean unownedTerritory = false;
-        for (String s : adjacentTerritories) {
-            if (gameBoard.getTerritoryRuler(s).equals(currPlayer)) {
-                gameBoard.printTerritoryStatus(s);
-                unownedTerritory = true;
-            }
-        }
-
-        if (!unownedTerritory) {
-            System.out.println("You don't own any adjacent territories to attack from");
-            return;
-        }
-
-        //asks for and processes the input for attacking territory
-        System.out.print("> ");
-        attackerTerritory = input.nextLine();
-
-        //checks if attacker actually owns this territory
-        if(!gameBoard.getTerritory(attackerTerritory).getRuler().equals(activePlayer.getName()))
-        {
-            System.out.println("You don't own this territory");
-            return;
-        }
-
-        //checks if attacking territory is adjacent to defending territory
-        if(!gameBoard.isAdjacentTerritory(attackerTerritory, defenderTerritory))
-        {
-            System.out.println("Those territories are not adjacent");
-            return;
-        }
-
-        //scans for input of attacker, stating dice amount
-        System.out.println("How many dice would you like to attack with (1 to 3)?");
-
-        int diceAttacker = 0;
-        while (true) {
-            try {
-                System.out.print("> ");
-                diceAttacker = input.nextInt();
-            } catch (Exception e) {
-                System.out.println("Number of dice must be a number.");
-            }
-
-            if (diceAttacker >= 1 && diceAttacker <= 3) {
-                break;
-            }
-            System.out.println("Attacker must have 1 to 3 dice");
-        }
-
-        //confirms if attacker has sufficient armies to use amount of dice
-        if(!(gameBoard.getArmy(attackerTerritory) >= diceAttacker + 1))
-        {
-            System.out.println("The attacker's territory does not have enough armies to use " + diceAttacker + " dice");
-            return;
-        }
-
-        System.out.println("How many dice would the defender like to use (1 to 2)?");
-
-        int diceDefender = 0;
-        while (true) {
-            try {
-                System.out.print("> ");
-                diceDefender = input.nextInt();
-            } catch (Exception e) {
-                System.out.println("Number of dice must be a number.");
-            }
-
-            if (diceDefender >= 1 && diceDefender <= 2) {
-                break;
-            }
-            System.out.println("Defender must have 1 to 2 dice");
-        }
-
-        if(!(gameBoard.getArmy(defenderTerritory) >= diceDefender)) {
-            System.out.println("The defender's territory does not have enough armies to use " + diceDefender + " dice");
-            return;
-        }
-
-        ArrayList<Integer> result = rollDie(diceAttacker, diceDefender); //stores result of dice roll
+        ArrayList<Integer> result = rollDie(attackersDice, defendersDice); //stores result of dice roll
         int attackerLosses = result.get(0);
         int defenderLosses = result.get(1);
 
@@ -228,7 +137,6 @@ public class Actions {
         gameBoard.removeTerritoryArmy(defenderTerritory, defenderLosses);
 
         if (gameBoard.getArmy(defenderTerritory) == 0) {
-            System.out.println(currPlayer + " has claimed " + defenderTerritory + " as their own!");
             String defender = gameBoard.getTerritoryRuler(defenderTerritory);
             gameBoard.removeTerritoryArmy(attackerTerritory, 1);
             gameBoard.addTerritoryArmy(defenderTerritory, 1);
@@ -237,10 +145,7 @@ public class Actions {
             removeEliminatedPlayer(defender);
         }
 
-        if (players.size() == 1) {
-            System.out.println(players.get(0).getName() + " Wins!");
-            System.exit(0);
-        }
+        new ActionEvent(this, gameBoard.getTerritory(defenderTerritory), gameBoard.getTerritoryRuler(defenderTerritory), result);
     }
 
     /**
@@ -257,15 +162,12 @@ public class Actions {
         int attackerLosses = 0;
         int defenderLosses = 0;
 
-        System.out.print("Attacker Rolled: ");
         for(int i = 0; i < numAttackDie; i++) {
             int roll = ran.nextInt(6) + 1;
             attackerRolls.add(roll);
             System.out.print(roll + " ");
         }
-        System.out.println();
 
-        System.out.print("Defender Rolled: ");
         for(int i = 0; i < numDefendDie; i++) {
             int roll = ran.nextInt(6) + 1;
             defenderRolls.add(roll);
@@ -301,8 +203,6 @@ public class Actions {
         result.add(attackerLosses);
         result.add(defenderLosses);
 
-        System.out.println("The attacker had " + result.get(0) + " loss(es).");
-        System.out.println("The defender had " + result.get(1) + " loss(es).");
 
         return result;
     }
@@ -333,56 +233,6 @@ public class Actions {
             activePlayerIndex = 0;
         }
 
-        activePlayer = players.get(activePlayerIndex);
-        printCurrentPlayer();
     }
 
-    /**
-     * Prints a textual representation of the current Player to the terminal
-     */
-    private void printCurrentPlayer() {
-        System.out.println(activePlayer.getName() + "'s Turn!");
-    }
-
-    /**
-     * Main play loop of the game. Terminates when there is only one player left
-     */
-    public void playGame() {
-        printCurrentPlayer();
-        boolean finished = false;
-        while (!finished) {
-            Command command = parser.getCommand();
-            finished = process(command);
-        }
-    }
-
-    /**
-     * Prints a textual representation of the board state to the terminal
-     */
-    private void printStatus()
-    {
-            gameBoard.printBoardStatus();
-    }
-
-    /**
-     * Processes a given command and executes the corresponding action
-     *
-     * @param command The command to be executed
-     * @return true if the game should be closed, false if the game should not be closed
-     */
-    private boolean process(Command command) {
-        boolean quit = false;
-        if (command.isUnknown()) {
-            parser.printCommands();
-        } else {
-            if (command.getCommandWord().equals("attack")) {
-                if (command.hasSecondWord()) attack(command.getSecondWord());
-                else System.out.println("Attack who?");
-            }
-            if (command.getCommandWord().equals("pass")) pass();
-            if (command.getCommandWord().equals("status")) printStatus();
-            if (command.getCommandWord().equals("quit")) quit = true;
-        }
-        return quit;
-    }
 }
