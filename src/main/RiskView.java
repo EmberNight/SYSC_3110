fe150bea3f7d4d30ab96d287dedbf55201fb021a
EmberNight <trautrim.tbt@hotmail.com>
2020-11-09 18:47:18 -0500

    chore (Project Cleanup): Added Comments

--- src/main/RiskView.java
+++ src/main/RiskView.java
@@ -4,7 +4,20 @@
  * Functional interface for use with Risk
  */
 public interface RiskView {
+    /**
+     * Updates the GUI when a attack has finished.
+     *
+     * @param ae Event that occurred
+     */
     void attackUpdate(RiskEvent ae);
+
+    /**
+     * Updates the gui when there is a new player.
+     */
     void passUpdate();
+
+    /**
+     * Updates the gui when the status pages needs updating.
+     */
     void updateStatus();
 }
