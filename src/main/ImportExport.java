469d80e462da26e74e896eeef14cd5cf8c0afbde
EmberNight <Embernight@protonmail.com>
2020-11-27 15:08:53 -0500

    chore (ImportExport.java): Setup for milestone 4
    - Created method

---
+++ src/main/ImportExport.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class ImportExport {
+    public void saveGame(File fileName) {
+        // Emmit
+        // Serialization
+    }
+    public void loadGame(File fileName) {
+        // Ashwin
+        // Serialization
+    }
+    public void importCustomMap(File fileName) {
+        // Tanner
+        // Use xml or jason with the PAX parser}
+    }
+}
