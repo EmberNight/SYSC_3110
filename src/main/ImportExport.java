5b95eab873118669eb0b0a67988e29eb202b628c
Em-kale <e.luhning@vitalpoint.ai>
2020-12-04 15:21:31 -0500

    Added loadGame and saveGame implementation in ImportExport, as well as getter methods and loadGame method in Risk class

--- src/main/ImportExport.java
+++ src/main/ImportExport.java
@@ -1,13 +1,44 @@
+import javax.swing.*;
+import java.io.*;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ImportExport {
-    public void saveGame(File fileName) {
-        // Emmit
+    private JFrame win;
+    public static void saveGame(Risk baseGame) {
+        // Emmitt
         // Serialization
+        //Step One. Serialize Risk object
+        try {
+            FileOutputStream fileOut = new FileOutputStream("gameSave.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(baseGame);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
     }
-    public void loadGame(File fileName) {
-        // Ashwin
-        // Serialization
+
+    public static void loadGame(Risk baseGame) {
+        Risk loadedGame;
+        try {
+            FileInputStream fileIn = new FileInputStream("gameSave.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            loadedGame = (Risk) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return;
+        }
+        baseGame.loadGame(loadedGame.getPhase(), loadedGame.getGameBoard(), loadedGame.getGameActions(), loadedGame.getPlayerList(),
+                loadedGame.getAdjacentTerritory(), loadedGame.getAttackerTerritory());
     }
     public void importCustomMap(File fileName) {
         // Tanner
